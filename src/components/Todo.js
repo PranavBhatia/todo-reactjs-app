8534a81f606cad39c3e9cd1ed234f6c344bc6e80
PranavBhatia <bhatiapranav1996@gmail.com>
2021-06-22 23:15:19 -0400

    Introducing State for Modals

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   function deleteHandler(params) {
-    console.log("clicked", props.text);
+    setModalIsOpen(true);
   }
 
   return (
@@ -11,7 +18,10 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
+    
   );
 }
 
